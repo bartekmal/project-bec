c81b443eb61d9ada77c264555a3ee7bf44966935
Bartosz Malecki <Bartosz Malecki bartosz.piotr.malecki@cern.ch>
2017-11-22 18:29:26 +0100

    Dataset test: minBias with microBias trigger VS FULL stream (PVMult)

---
+++ DaVinciDev_v36r7/Phys/HBTCorrelations/job/myjob/test/pPb/microBias/run.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+source /cvmfs/lhcb.cern.ch/lib/LbLogin.sh -c x86_64-slc6-gcc48-opt
+./../../../../../../../run gaudirun.py ../../../options/HBTCorrelations.py ../../../data/test_protonion.py > pPb.log &
