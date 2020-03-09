37c0d27ceebb34fba3be0eb57fa67bb986f73148
Bartosz Malecki <Bartosz Malecki bartosz.piotr.malecki@cern.ch>
2020-03-09 16:08:21 +0100

    Fit: prepare generic scripts for fitting in mult/kT bins.
    - ensure proper error propagation when dividing histograms
    
    - use the full B-S model with bkg description (no DR)
    
    - use bkg params from DATA UNLIKE (with scaling)
    
    - plot trends and diffs of the fit parameters

--- code/scripts/divideHistogramsInBins.C
+++ code/scripts/divideHistogramsInBins.C
@@ -1,69 +1,144 @@
-TH1D* divideHistograms( const TH1D* h1, const TH1D* h2, const TString hOutName = "", const bool isDR = false ){
-  
-  //create a histogram with identical binning
-  TH1D* hOut = (TH1D*)h1->Clone( hOutName );
-  hOut->Sumw2();
-  
-  //set title
-  hOut->SetTitle( isDR ? TString( TString( hOut->GetName() ) + "; Q [GeV]; DR(Q)" ) : TString( TString( hOut->GetName() ) + "; Q [GeV]; C(Q)" ) );
+const Double_t Pi = TMath::Pi();
+const Double_t TwoPi = 2 * TMath::Pi();
+const Double_t MassPion = 0.140;
+const Double_t FineStructureConstant = 1.0 / 137.0;
 
-  //divide
-  if( !isDR ) hOut->Divide( h1, h2, h2->Integral(), h1->Integral() );
-  else hOut->Divide( h1, h2 );
+Double_t gamovFactorForPionPair(const Double_t &Q, const bool &sameSign)
+{
 
-  return hOut;
-     
+  const int sign = sameSign ? 1 : -1;
+
+  const Double_t tmpEtaTwoPi = TwoPi * MassPion * FineStructureConstant / Q;
+
+  return sign * tmpEtaTwoPi / (exp(sign * tmpEtaTwoPi) - 1);
 }
 
-void divideHistogramsInBins( const TString file1, const TString h1BaseName, const TString file2, const TString h2BaseName, const TString fileOut, const TString hOutBaseName, const int isDR = 0, const int nrBinsMult = 6, const int nrBinsKt = 0, TString hCommonEndName = "" ){
-  
-  //prepare input/output files
-  auto f1 = new TFile( TString( file1 ),"READ" );
-  auto f2 = new TFile( TString( file2 ),"READ" );
-  auto fOut = new TFile( TString( fileOut ),"UPDATE" );
+Double_t fullCoulombCorrectionForPionPair(const Double_t &Q, const bool &sameSign, const Double_t assumedR = 2.)
+{
 
-  for( int i = 0; i < nrBinsMult; ++i ){
+  const int sign = sameSign ? 1 : -1;
 
-    if ( nrBinsKt != 0 ) {
+  const Double_t tmpDzeta = MassPion * FineStructureConstant / Q;
 
-      for( int j = 0; j < nrBinsKt; ++j ){
+  const Double_t weight = (1 + sign * Pi * tmpDzeta * Q * assumedR / (1.26 + Q * assumedR));
 
-        TString h1Name = ( TString( ( TString( h1BaseName+"_" ) += ( i+1 ) ) + "_" ) += ( j+1 ) )  +  hCommonEndName;
-        TString h2Name = ( TString( ( TString( h2BaseName+"_" ) += ( i+1 ) ) + "_" ) += ( j+1 ) ) +  hCommonEndName;
-        TString hOutName = ( TString( ( TString( hOutBaseName+"_" ) += ( i+1 ) ) + "_" ) += ( j+1 ) ) +  hCommonEndName;
+  return gamovFactorForPionPair(Q, sameSign) * weight;
+}
 
-        TH1D* h1 = (TH1D*)f1->Get(h1Name);
-        TH1D* h2 = (TH1D*)f2->Get(h2Name);
+TH1D *divideHistograms(TH1D *h1, TH1D *h2, const TString hOutName = "", const bool isDR = false)
+{
 
-        auto hOut = divideHistograms( h1, h2, hOutName, isDR ); 
-        hOut->Write();
+  // create a histogram with identical binning
+  TH1D *hOut = (TH1D *)h1->Clone(hOutName);
+  hOut->SetTitle(isDR ? TString(TString(hOut->GetName()) + "; Q [GeV]; DR(Q)") : TString(TString(hOut->GetName()) + "; Q [GeV]; C(Q)"));
+  hOut->Reset();
 
-        delete h1;
-        delete h2;
-        delete hOut;
+  // deal with histogram errors before dividing
+  if (h1->GetSumw2N() == 0)
+  {
+    h1->Sumw2();
+  }
 
+  if (h2->GetSumw2N() == 0)
+  {
+    h2->Sumw2();
+  }
+
+  // divide
+  if (!isDR)
+    hOut->Divide(h1, h2, h2->Integral(), h1->Integral());
+  else
+    hOut->Divide(h1, h2);
+
+  // calculate errors by hand
+  if (!isDR)
+  {
+
+    // get histograms normalisation
+    const float n1 = h1->Integral();
+    const float n2 = h2->Integral();
+
+    for (int i = 1; i < hOut->GetNbinsX() + 1; i++)
+    {
+      // get bin contents and errors
+      const float q1 = h1->GetBinContent(i);
+      const float q2 = h2->GetBinContent(i);
+      const float e1 = h1->GetBinError(i);
+      const float e2 = h2->GetBinError(i);
+
+      // calculate error
+      const float eOut = n2 / n1 * (1. / q2) * TMath::Sqrt(TMath::Power(e1, 2) + TMath::Power(q1 / q2 * e2, 2));
+
+      // check for differences
+      if (fabs(eOut - hOut->GetBinError(i)) > 0.00001)
+      {
+        std::cout << "Hello there!: errors propagation in the correlation function gives unexpected results." << std::endl;
+        std::cout << eOut << "\t" << hOut->GetBinError(i) << std::endl;
       }
 
-    } else {
+      // hOut->SetBinError(i, eOut);
+    }
+  }
 
-      TString h1Name = ( TString( ( TString( h1BaseName+"_" ) += ( i+1 ) ) + "_0" ) )  +  hCommonEndName;
-      TString h2Name = ( TString( ( TString( h2BaseName+"_" ) += ( i+1 ) ) + "_0" ) ) +  hCommonEndName;
-      TString hOutName = ( TString( ( TString( hOutBaseName+"_" ) += ( i+1 ) ) + "_0" ) ) +  hCommonEndName;
+  return hOut;
+}
 
-      TH1D* h1 = (TH1D*)f1->Get(h1Name);
-      TH1D* h2 = (TH1D*)f2->Get(h2Name);
+// get a full histogram name in the given bin
+TString getFullHistogramName(const TString baseNameBegin, const TString baseNameEnd, const bool multBinsOnly, const int binNrMult, const int binNrKt = 0)
+{
+  return multBinsOnly ? TString(TString(TString(baseNameBegin + "_") += (binNrMult + 1)) + "_0") + baseNameEnd : TString(TString(TString(TString(baseNameBegin + "_") += (binNrMult + 1)) + "_") += (binNrKt + 1)) + baseNameEnd;
+}
 
-      auto hOut = divideHistograms( h1, h2, hOutName, isDR ); 
+void divideHistogramsInBins(const TString file1, const TString h1BaseName, const TString file2, const TString h2BaseName, const TString fileOut, const TString hOutBaseName, const int isDR = 0, const int nrBinsMult = 6, const int nrBinsKt = 0, TString hCommonEndName = "", const int flagCorrectCoulomb = 0, const int isLikePair = 0)
+{
+
+  // prepare settings for type of binning
+  const bool isMultBinsOnly = (nrBinsKt == 0) ? true : false;
+
+  const int nrBinsKtForLoops = isMultBinsOnly ? 1 : nrBinsKt;
+
+  //prepare input/output files
+  auto f1 = new TFile(TString(file1), "READ");
+  auto f2 = new TFile(TString(file2), "READ");
+  auto fOut = new TFile(TString(fileOut), "UPDATE");
+
+  for (int i = 0; i < nrBinsMult; ++i)
+  {
+
+    for (int j = 0; j < nrBinsKtForLoops; ++j)
+    {
+
+      TString h1Name = isMultBinsOnly ? getFullHistogramName(h1BaseName, hCommonEndName, isMultBinsOnly, i) : getFullHistogramName(h1BaseName, hCommonEndName, isMultBinsOnly, i, j);
+      TString h2Name = isMultBinsOnly ? getFullHistogramName(h2BaseName, hCommonEndName, isMultBinsOnly, i) : getFullHistogramName(h2BaseName, hCommonEndName, isMultBinsOnly, i, j);
+      TString hOutName = isMultBinsOnly ? getFullHistogramName(hOutBaseName, hCommonEndName, isMultBinsOnly, i) : getFullHistogramName(hOutBaseName, hCommonEndName, isMultBinsOnly, i, j);
+
+      TH1D *h1 = (TH1D *)f1->Get(h1Name);
+      TH1D *h2 = (TH1D *)f2->Get(h2Name);
+
+      auto hOut = divideHistograms(h1, h2, hOutName, isDR);
+
+      if (flagCorrectCoulomb)
+      {
+
+        hOut->Sumw2();
+
+        for (int i = 1; i < hOut->GetNbinsX() + 1; i++)
+        {
+
+          const auto currentBinContent = hOut->GetBinContent(i);
+          const auto currentCorrectionFactor = 1. / fullCoulombCorrectionForPionPair(hOut->GetBinCenter(i), isLikePair);
+          hOut->SetBinContent(i, currentBinContent * currentCorrectionFactor);
+        }
+      }
+
       hOut->Write();
 
       delete h1;
       delete h2;
       delete hOut;
-
     }
-
   }
-  
+
   //cleanup
   f1->Close();
   f2->Close();
@@ -72,5 +147,4 @@ void divideHistogramsInBins( const TString file1, const TString h1BaseName, cons
   delete f1;
   delete f2;
   delete fOut;
-
 }
---
+++ code/scripts/fitInBins.C
@@ -0,0 +1,412 @@
+/*-------------- configure fit -------------------*/
+
+const Double_t R_eff = 2.0;
+const Double_t fitRangeLikeMin = 0.05;
+const Double_t fitRangeUnlikeMin = 0.25;
+const Double_t fitRangeMcMin = 0.25;
+const Double_t fitRangeBkgScaleMin = 0.4;
+const Double_t fitRangeMax = 2.0;
+const Int_t fitNrOfParams = 9;
+
+/*-------------- define some constants -------------------*/
+
+const Double_t Pi = TMath::Pi();
+const Double_t TwoPi = 2 * TMath::Pi();
+const Double_t MassPion = 0.140;
+const Double_t FineStructureConstant = 1.0 / 137.0;
+
+/*-------------- define elementary functions -------------------*/
+
+Double_t funcGamov(const Double_t &Q, const bool &sameSign)
+{
+
+    const int sign = sameSign ? 1 : -1;
+
+    const Double_t tmpEtaTwoPi = TwoPi * MassPion * FineStructureConstant / Q;
+
+    return sign * tmpEtaTwoPi / (exp(sign * tmpEtaTwoPi) - 1);
+}
+
+Double_t funcBS(const Double_t &Q, const Double_t &assumedR, const bool &sameSign)
+{
+    const Double_t tmpDzeta = MassPion * FineStructureConstant / Q;
+
+    const Double_t weight = (1 + Pi * tmpDzeta * Q * assumedR / (1.26 + Q * assumedR));
+
+    return funcGamov(Q, sameSign) * weight;
+}
+
+/*----------------------------------------------------------------*/
+
+/*-------------- define partial fit functions -------------------*/
+
+Double_t funcBkg(Double_t *x, Double_t *par)
+{
+    return (1 + par[0] * x[0] + par[1] * par[4] * exp(-pow(par[2] * x[0], par[3])));
+}
+
+Double_t funcBEC(Double_t *x, Double_t *par)
+{
+    return (1 + exp(-x[0] * par[0]));
+}
+
+Double_t funcCoulombLike(Double_t *x, Double_t *par)
+{
+    return funcBS(x[0], par[0], true);
+}
+
+Double_t funcCoulombUnlike(Double_t *x, Double_t *par)
+{
+    return funcBS(x[0], par[0], false);
+}
+
+/*-------------- define final fit functions -------------------*/
+
+Double_t funcLevy(Double_t *x, Double_t *par)
+{
+    return par[0] * (1 + par[1] * exp(-(x[0] * par[2]))) * (1 + x[0] * par[3]);
+}
+
+Double_t funcFullLike(Double_t *x, Double_t *par)
+{
+    return par[0] * (1 - par[1] + par[1] * funcCoulombLike(x, &par[3]) * funcBEC(x, &par[2])) * funcBkg(x, &par[4]);
+}
+
+Double_t funcFullUnlike(Double_t *x, Double_t *par)
+{
+    return par[0] * (1 - par[1] + par[1] * funcCoulombUnlike(x, &par[3])) * funcBkg(x, &par[4]);
+}
+
+/*----------------------------------------------------------------*/
+
+TString getFitResultName(const TString hName, const TString fName)
+{
+    return hName + TString("_") + fName;
+}
+
+Double_t getBkgScalingFactor(const TFitResult *fitResultMain, const TFitResult *fitResultRef, const int &parameterId)
+{
+    return float(fitResultMain->Parameter(parameterId)) / fitResultRef->Parameter(parameterId);
+}
+
+TFitResultPtr doFit(TH1D *h, TF1 *f, const TString fitOpts = "S0R")
+{
+    TFitResultPtr fitResult = h->Fit(f->GetName(), fitOpts);
+    fitResult->Write(getFitResultName(h->GetName(), f->GetName()));
+
+    return fitResult;
+}
+
+void printDescription(const TString dataType, const int currentMultBin, const int currentKtBin)
+{
+
+    Double_t commPosX = 0.55;
+    Double_t commPosY = 0.35;
+    TLatex comments;
+    comments.SetNDC();
+    comments.SetTextSize(0.035);
+    comments.DrawLatex(commPosX, commPosY, dataType);
+    if (currentMultBin != 0)
+    {
+        comments.DrawLatex(commPosX, commPosY - 0.05, TString("Multiplicity bin: ") += currentMultBin);
+    }
+    if (currentKtBin != 0)
+    {
+        comments.DrawLatex(commPosX, commPosY - 0.10, TString("k_{T} bin: ") += currentKtBin);
+    }
+}
+
+void drawPull(TH1D *hFit, TF1 *fFit, const TString drawOpts = "BSAME")
+{
+    // create a histogram with identical binning
+    TH1D *hPull = (TH1D *)hFit->Clone(TString(hFit->GetName()) + "_pull");
+    hPull->SetTitle("pull;Q [GeV]; pull");
+    hPull->Reset();
+
+    //make pull
+    for (int i = 1; i < hFit->GetNbinsX() + 1; ++i)
+    {
+
+        const Double_t expected = fFit->Eval(hFit->GetBinCenter(i));
+        const Double_t observed = hFit->GetBinContent(i);
+        const Double_t sigma = hFit->GetBinError(i);
+
+        const Double_t pullValue = (sigma != 0.) ? (observed - expected) / sigma : 0.;
+
+        hPull->SetBinContent(i, pullValue);
+    }
+
+    //draw
+    hPull->SetLabelSize(0.06, "x"); // X numbers label size ()
+    hPull->SetLabelSize(0.06, "y"); // Y numbers label size ()
+    hPull->SetTitleSize(0.07, "x"); // X title size ()
+    hPull->SetTitleSize(0.07, "y"); // Y title size ()
+    hPull->GetYaxis()->SetTitleOffset(0.5);
+    //hPull->SetBarWidth( 0.1 );
+    hPull->SetFillColor(kBlue);
+    hPull->GetYaxis()->SetRangeUser(-6, 6);
+    hPull->Draw(drawOpts);
+
+    //draw lines
+    TLine *lineUp = new TLine(0, 3, 2, 3);
+    lineUp->SetLineColor(kRed);
+    lineUp->Draw("SAME");
+    TLine *lineDown = new TLine(0, -3, 2, -3);
+    lineDown->SetLineColor(kRed);
+    lineDown->Draw("SAME");
+}
+
+void setStyle()
+{
+    //style
+    gROOT->Reset();                  // Reset options
+    gStyle->SetPadGridX(kFALSE);     // No grid in x (0)
+    gStyle->SetPadGridY(kFALSE);     // No grid in y (0)
+    gStyle->SetOptTitle(kFALSE);     // No title
+    gStyle->SetStatBorderSize(0);    // Stats box shadow size (2)
+    gStyle->SetStatColor(18);        // Stats box fill color (0)
+    gStyle->SetStatFont(62);         // Stats font style (62)
+    gStyle->SetStatH(0.1);           // Stats box height
+    gStyle->SetStatW(0.15);          // Stats box width
+    gStyle->SetStatX(0.91);          // Stats box x coordinate
+    gStyle->SetStatY(0.91);          // Stats box y coordinate
+    gStyle->SetStatStyle(1001);      // Stat box fill style
+    gStyle->SetStatTextColor(1);     // Stat text color (1)
+    gStyle->SetOptStat(0);           // No statistics (0) (1000001110)
+    gStyle->SetOptFit(111);          // No fit box (0) (111)
+    gStyle->SetFrameBorderMode(0);   // No red box
+    gStyle->SetHistFillColor(0);     // Histogram fill color (0) (18)
+    gStyle->SetHistFillStyle(1001);  // Histogram fill style (1001) (0)
+    gStyle->SetHistLineColor(kBlue); // Histogram line color (1)
+    gStyle->SetHistLineStyle(0);     // Histogram line style (0)
+    gStyle->SetHistLineWidth(1);     // Histogram line width (1.0)
+    gStyle->SetMarkerStyle(21);      // Marker style (0)
+    gStyle->SetMarkerColor(kBlack);  // Marker color (1)
+    gStyle->SetMarkerSize(1.2);      // Marker size ()
+    gStyle->SetLineColor(kBlack);    // Line color (1)
+    gStyle->SetLineWidth(1.0);       // Line width (1.0)
+    gStyle->SetTextSize(0.07);       // Text size (1.0)
+    gStyle->SetLabelSize(0.03, "x"); // X numbers label size ()
+    gStyle->SetLabelSize(0.03, "y"); // Y numbers label size ()
+    gStyle->SetTitleSize(0.04, "x"); // X title size ()
+    gStyle->SetTitleSize(0.04, "y"); // Y title size ()
+    gStyle->SetErrorX(0);            // No errors along x
+    gROOT->ForceStyle();
+}
+
+// get a full histogram name in the given bin
+TString getFullHistogramName(const TString baseNameBegin, const TString baseNameEnd, const bool multBinsOnly, const int binNrMult, const int binNrKt = 0)
+{
+    return multBinsOnly ? TString(TString(TString(baseNameBegin + "_") += (binNrMult + 1)) + "_0") + baseNameEnd : TString(TString(TString(TString(baseNameBegin + "_") += (binNrMult + 1)) + "_") += (binNrKt + 1)) + baseNameEnd;
+}
+
+void drawHistogram(TH1D *h, const int color = kBlue, const TString drawOpts = "ESAME", const Double_t yMin = 0.5, const Double_t yMax = 1.6)
+{
+
+    //draw histogram
+    h->GetYaxis()->SetRangeUser(yMin, yMax);
+    h->SetMarkerColor(color);
+    h->SetLineColor(color);
+    h->GetYaxis()->SetTitleOffset(1.2);
+
+    h->Draw(drawOpts);
+}
+
+void fitInBins(const TString inputFile, const TString hMainNameBase, const bool flagIsMc = false, const bool flagIsUnlike = false, const TString dataType = "", const int nrBinsMult = 6, const int nrBinsKt = 0, TString hCommonEndName = "", const bool flagDoFit = true, const TString inputFileRef = "", const TString hRefNameBase = "", const TString refType = "", const bool flagDrawRef = false, const bool flagUseBkgFromRef = false, const bool flagIsBkgScaling = false, const bool flagUseBkgScaling = false, const TString funcNameBkgScalingMain = "", const TString funcNameBkgScalingRef = "")
+{
+
+    setStyle();
+
+    // prepare settings for type of binning and histogram type
+    const bool isMultBinsOnly = (nrBinsKt == 0) ? true : false;
+    const int nrBinsKtForLoops = isMultBinsOnly ? 1 : nrBinsKt;
+
+    const TString binsType = isMultBinsOnly ? TString("mult") : TString("kT");
+    const TString histType = flagIsUnlike ? TString("UNLIKE") : TString("LIKE");
+    const TString funcName = flagIsBkgScaling ? TString("funcBkgScaling") : TString("funcMain");
+    const TString title = funcName + "_" + binsType + "_" + histType;
+
+    // prepare canvas
+    const int padSize = 1200;
+    const int canvasSizeHeight = flagDoFit ? nrBinsMult * (padSize * 1.5) : nrBinsMult * padSize;
+    const int canvasSizeWidth = isMultBinsOnly ? padSize : nrBinsKtForLoops * padSize;
+    TCanvas *tc = new TCanvas(title, title, canvasSizeWidth, canvasSizeHeight);
+    tc->Divide(nrBinsKtForLoops, nrBinsMult);
+
+    // prepare I/O files
+    TFile *fIn = new TFile(inputFile, "READ");
+    TFile *fOut = new TFile("fitResults.root", "UPDATE");
+
+    // optional file with reference histograms / fits
+    TFile *fInRef = flagDrawRef ? new TFile(inputFileRef, "READ") : nullptr;
+    TFile *fInRefFitResults = flagUseBkgFromRef ? new TFile(TString(inputFileRef).ReplaceAll("correlations.root", "fitResults.root").ReplaceAll("correlations", "fits"), "READ") : nullptr;
+
+    // loop over all bins of mult / kT
+    for (int i = 0; i < nrBinsMult; ++i)
+    {
+
+        for (int j = 0; j < nrBinsKtForLoops; ++j)
+        {
+
+            // prepare pads
+            tc->cd((j + nrBinsKtForLoops * i) + 1);
+
+            TLegend *tl = new TLegend(0.55, 0.50, 0.85, 0.65);
+
+            TPad *p1 = new TPad("p1", "p1", 0., 0.33, 1., 1.);
+            TPad *p2 = new TPad("p2", "p2", 0., 0., 1., 0.33);
+            p2->SetBottomMargin(0.15);
+            p1->Draw();
+            p2->Draw();
+
+            // get histograms
+            TString hMainName = isMultBinsOnly ? getFullHistogramName(hMainNameBase, hCommonEndName, isMultBinsOnly, i) : getFullHistogramName(hMainNameBase, hCommonEndName, isMultBinsOnly, i, j);
+            TH1D *hMain = (TH1D *)fIn->Get(hMainName);
+
+            //get the reference histogram and fit result if available
+            const auto hRefName = isMultBinsOnly ? getFullHistogramName(hRefNameBase, hCommonEndName, isMultBinsOnly, i) : getFullHistogramName(hRefNameBase, hCommonEndName, isMultBinsOnly, i, j);
+            TH1D *hRef = flagDrawRef ? (TH1D *)fInRef->Get(hRefName) : nullptr;
+            auto fitResultRef = flagUseBkgFromRef ? (TFitResult *)fInRefFitResults->Get(getFitResultName(hRefName, "funcMain")) : nullptr;
+            fOut->cd();
+
+            // draw histograms
+            p1->cd();
+            drawHistogram(hMain, kBlue);
+            tl->AddEntry(hMain, histType, "pe");
+
+            // draw a reference histogram if required
+            if (flagDrawRef)
+            {
+                drawHistogram(hRef, kGreen);
+                tl->AddEntry(hRef, refType, "pe");
+            }
+
+            // perform a fit if required
+            if (flagDoFit)
+            {
+                // create a function relevant for the histogram type (from a generic form)
+                const auto fitRangeMin = flagIsBkgScaling ? fitRangeBkgScaleMin : flagIsMc ? fitRangeMcMin : flagIsUnlike ? fitRangeUnlikeMin : fitRangeLikeMin;
+                TF1 *funcMain = new TF1(funcName, flagIsUnlike ? funcFullUnlike : funcFullLike, fitRangeMin, fitRangeMax, fitNrOfParams);
+
+                // settings common for all histogram types
+                funcMain->SetParNames("N", "#lambda", "R", "R_{eff} [fm]", "#delta", "#lambda_{bkg}", "R_bkg", "alpha_bkg", "scaleFactor(#lambda_{bkg})");
+                funcMain->SetParameters(1.0, 0.5, 7., R_eff, 0.1, 0.1, 1.5, 2.0, 1.0);
+                funcMain->FixParameter(3, R_eff);
+                funcMain->FixParameter(7, 2);
+                funcMain->FixParameter(8, 1.0);
+
+                funcMain->SetParLimits(0, 0.5, 1.5);
+                funcMain->SetParLimits(1, 0., 2.0);
+                funcMain->SetParLimits(2, 0., 50.);
+                funcMain->SetParLimits(4, -0.5, 0.5);
+                funcMain->SetParLimits(5, 0., 10.);
+                funcMain->SetParLimits(6, 0., 50.);
+
+                // settings for the specific histogram types (keep an eye on 'priority': bkgScaling -> MC -> UNLIKE -> LIKE)
+                // set the unused (fixed) parameters to some neutral value (e.g. 0)
+                if (flagIsBkgScaling)
+                {
+                    funcMain->FixParameter(1, 0);
+                    funcMain->FixParameter(2, 0);
+                    funcMain->FixParameter(3, 0);
+                }
+                else if (flagIsMc)
+                {
+                    funcMain->FixParameter(1, 0);
+                    funcMain->FixParameter(2, 0);
+                    funcMain->FixParameter(3, 0);
+                }
+                else if (flagIsUnlike)
+                {
+                    // works also with the following settings, but is more unstable (lambda is at 0 limit anyway)
+                    // funcMain->SetParameter(1, 0.0);
+                    // funcMain->SetParLimits(1, 0., 1.0);
+                    funcMain->FixParameter(1, 0);
+                    funcMain->FixParameter(2, 0);
+                }
+
+                // use the bkg parametrisation from a reference fit
+                if (flagUseBkgFromRef)
+                {
+                    // use the fit only if it was valid
+                    if (fitResultRef->IsValid())
+                    {
+                        // scale the bkg parameters if needed
+                        if (flagUseBkgScaling)
+                        {
+                            auto fitResultBkgScalingMain = (TFitResult *)fInRefFitResults->Get(getFitResultName(hMainName, funcNameBkgScalingMain));
+                            auto fitResultBkgScalingRef = (TFitResult *)fInRefFitResults->Get(getFitResultName(hRefName, funcNameBkgScalingRef));
+
+                            if (fitResultBkgScalingMain->IsValid() && fitResultBkgScalingRef->IsValid())
+                            {
+                                // use the lambda_bkg parameter from the reference fit (scaled by the difference between LIKE/UNLIKE)
+                                funcMain->FixParameter(5, fitResultRef->Parameter(5));
+                                funcMain->FixParameter(8, getBkgScalingFactor(fitResultBkgScalingMain, fitResultBkgScalingRef, 5));
+                            }
+                            else
+                            {
+                                std::cout << "One of fits for the bkg parameters scaling is not valid: \n\t" << fitResultBkgScalingMain << "\n\t" << fitResultBkgScalingRef << std::endl;
+                            }
+                        }
+
+                        // use the R_bkg parameter from the reference fit
+                        funcMain->FixParameter(6, fitResultRef->Parameter(6));
+                    }
+                    else
+                    {
+                        std::cout << "Fit with the bkg parameters is not valid: \n\t" << hMain << "\n\t" << funcMain << std::endl;
+                    }
+
+                    fOut->cd();
+                }
+
+                // do a fit and draw if it is valid
+                const auto fitResult = doFit(hMain, funcMain);
+                if (fitResult->IsValid())
+                {
+                    funcMain->SetLineColor(kRed);
+                    funcMain->SetLineWidth(funcMain->GetLineWidth() * 0.5);
+                    funcMain->Draw("SAME");
+                    tl->AddEntry(funcMain, histType + " (fit)", "l");
+
+                    // draw the pull distribution
+                    p2->cd();
+                    drawPull(hMain, funcMain);
+                    p1->cd();
+                }
+                else
+                {
+                    const auto curStyle = gStyle->GetOptFit();
+                    gStyle->SetOptFit(0);
+                    hMain->UseCurrentStyle();
+                    gStyle->SetOptFit(curStyle);
+                    std::cout << "Fit is not valid: \n\t" << hMain << "\n\t" << funcMain << std::endl;
+                }
+            }
+
+            // add description / legend
+            printDescription(dataType, i + 1, j + 1);
+            tl->Draw("SAME");
+        }
+    }
+
+    tc->SaveAs(".pdf");
+
+    // I/O close & cleanup
+    fIn->Close();
+    delete fIn;
+
+    fOut->Close();
+    delete fOut;
+
+    if (fInRef != nullptr)
+    {
+        fInRef->Close();
+        delete fInRef;
+    }
+    if (fInRefFitResults != nullptr)
+    {
+        fInRefFitResults->Close();
+        delete fInRefFitResults;
+    }
+}
