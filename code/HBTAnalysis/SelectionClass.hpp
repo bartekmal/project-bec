9cce408c5e2f7c75697a8bbc080c28a09e095ea3
Bartosz Malecki <Bartosz Malecki bartosz.piotr.malecki@cern.ch>
2021-07-14 19:16:47 +0200

    Selection: zPV stricter (20%)

--- code/HBTAnalysis/SelectionClass.hpp
+++ code/HBTAnalysis/SelectionClass.hpp
@@ -92,8 +92,8 @@ public:
 inline void SelectionClass::initializeSelectionCuts()
 {
 
-    this->m_zPvMin = -192.;
-    this->m_zPvMax = 72.;
+    this->m_zPvMin = -128.;
+    this->m_zPvMax = 48.;
 
     this->m_trackChi2Cut = 2.0f;
     this->m_ipCut = 0.4f;
