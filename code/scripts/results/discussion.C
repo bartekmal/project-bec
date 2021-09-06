449f5c2d48ba7b93f2933089bcd94b598df851de
Bartosz Malecki <Bartosz Malecki bartosz.piotr.malecki@cern.ch>
2021-09-07 01:43:55 +0200

    Discussion:
    - add pp plots
    - fix multiplicity ranges in discussion
    - fix labels in mult scaling
    - labels N_VELO -> N_rec.

--- code/scripts/results/discussion.C
+++ code/scripts/results/discussion.C
@@ -148,7 +148,7 @@ void discussionGeneric(const PlotConfig &plot, const unsigned int &flagStyle)
             const auto funcName = TString("funcMult");
 
             auto fTrend = std::make_unique<TF1>(funcName, (plot.flagFit == 1) ? "[0] + [1]*pow(x,1./3.)" : "[0] + [1]*x", plot.axisRangeX.first, plot.axisRangeX.second);
-            fTrend->SetParNames("a", "b");
+            fTrend->SetParNames("b", "a");
             fTrend->SetParameters(0.0, 1.0);
             fTrend->SetParLimits(0, -10.0, 10.0);
             fTrend->SetParLimits(1, 0, 10);
@@ -221,15 +221,18 @@ void discussion(const std::string basePath, const unsigned int &flagStyle = HBT:
 
     const std::vector<PlotConfig> plots{
         // basic mult dependence
-        PlotConfig("lambda_mult_pPb", ";#font[12]{N_{rec}};#lambda", "#lambda : mult (pPb)", std::make_pair(0., multMax), std::make_pair(0., lambdaMax), "lambda", {basePath + "/main/output/systematics/RD_pPb/lambda_multReconstructed.csv"}, {"#font[12]{p}Pb 5.02 TeV"}, {false}),
-        PlotConfig("lambda_mult_Pbp", ";#font[12]{N_{rec}};#lambda", "#lambda : mult (Pbp)", std::make_pair(0., multMax), std::make_pair(0., lambdaMax), "lambda", {basePath + "/main/output/systematics/RD_Pbp/lambda_multReconstructed.csv"}, {"Pb#font[12]{p} 5.02 TeV"}, {false}),
-        PlotConfig("radius_mult_pPb", ";#font[12]{N_{rec}};#font[12]{R} [fm]", "R : mult (pPb)", std::make_pair(0., multMax), std::make_pair(0.0, radiusMax), "radius", {basePath + "/main/output/systematics/RD_pPb/radius_multReconstructed.csv"}, {"#font[12]{p}Pb 5.02 TeV"}, {true}, 1),
-        PlotConfig("radius_mult_Pbp", ";#font[12]{N_{rec}};#font[12]{R} [fm]", "R : mult (Pbp)", std::make_pair(0., multMax), std::make_pair(0.0, radiusMax), "radius", {basePath + "/main/output/systematics/RD_Pbp/radius_multReconstructed.csv"}, {"Pb#font[12]{p} 5.02 TeV"}, {true}, 1),
+        PlotConfig("lambda_mult_pPb", ";#font[12]{N}_{VELO};#lambda", "#lambda : mult (pPb)", std::make_pair(0., multMax), std::make_pair(0., lambdaMax), "lambda", {basePath + "/main/output/systematics/RD_pPb/lambda_multReconstructed.csv"}, {"#font[12]{p}Pb 5.02 TeV"}, {false}),
+        PlotConfig("lambda_mult_Pbp", ";#font[12]{N}_{VELO};#lambda", "#lambda : mult (Pbp)", std::make_pair(0., multMax), std::make_pair(0., lambdaMax), "lambda", {basePath + "/main/output/systematics/RD_Pbp/lambda_multReconstructed.csv"}, {"Pb#font[12]{p} 5.02 TeV"}, {false}),
+        PlotConfig("radius_mult_pPb", ";#font[12]{N}_{VELO};#font[12]{R} [fm]", "R : mult (pPb)", std::make_pair(0., multMax), std::make_pair(0.0, radiusMax), "radius", {basePath + "/main/output/systematics/RD_pPb/radius_multReconstructed.csv"}, {"#font[12]{p}Pb 5.02 TeV"}, {true}),
+        PlotConfig("radius_mult_Pbp", ";#font[12]{N}_{VELO};#font[12]{R} [fm]", "R : mult (Pbp)", std::make_pair(0., multMax), std::make_pair(0.0, radiusMax), "radius", {basePath + "/main/output/systematics/RD_Pbp/radius_multReconstructed.csv"}, {"Pb#font[12]{p} 5.02 TeV"}, {true}),
         // compare data samples
         PlotConfig("radius_mult_compareSystems", ";#font[12]{N_{rec}};#font[12]{R} [fm]", "R : data samples", std::make_pair(0., multMax), std::make_pair(0.0, radiusMax), "radius", {basePath + "/main/output/systematics/RD_pPb/radius_multReconstructed.csv", basePath + "/main/output/systematics/RD_Pbp/radius_multReconstructed.csv", externalPath + "/pp_lhcb_radius_multReconstructed.csv"}, {"#font[12]{p}Pb 5.02 TeV", "Pb#font[12]{p} 5.02 TeV", "#font[12]{pp} 7 TeV"}, {true, true, false}, 1),
         PlotConfig("lambda_mult_compareSystems", ";#font[12]{N_{rec}};#lambda", "#lambda : data samples", std::make_pair(0., multMax), std::make_pair(0., lambdaMax), "lambda", {basePath + "/main/output/systematics/RD_pPb/lambda_multReconstructed.csv", basePath + "/main/output/systematics/RD_Pbp/lambda_multReconstructed.csv", externalPath + "/pp_lhcb_lambda_multReconstructed.csv"}, {"#font[12]{p}Pb 5.02 TeV", "Pb#font[12]{p} 5.02 TeV", "#font[12]{pp} 7 TeV"}, {false, false, false}),
-        // pp : LHCb vs ATLAS
-        PlotConfig("radius_mult_rapidity", ";#font[12]{N_{ch}};#font[12]{R} [fm]", "R : rapidity", std::make_pair(0., 250.0), std::make_pair(0.0, 3.5), "radius", {externalPath + "/pp_atlas_radius_multUnfolded_MB.csv", externalPath + "/pp_atlas_radius_multUnfolded_HM.csv", externalPath + "/pp_lhcb_radius_multUnfolded.csv"}, {"ATLAS #font[12]{pp} 7 TeV (MB)", "ATLAS #font[12]{pp} 7 TeV (HM)", "LHCb #font[12]{pp} 7 TeV"}, {false, false, false}, 0),
+        // pp : LHCb vs ATLAS (manual: ? wider syst errors)
+        PlotConfig("radius_mult_rapidity", ";#font[12]{N_{ch}};#font[12]{R} [fm]", "R : rapidity", std::make_pair(0., 250.0), std::make_pair(0.0, 3.5), "radius", {externalPath + "/pp_atlas_radius_multUnfolded_MB.csv", externalPath + "/pp_atlas_radius_multUnfolded_HM.csv", externalPath + "/pp_lhcb_radius_multUnfoldedAtlas.csv"}, {"ATLAS #font[12]{pp} 7 TeV (MB)", "ATLAS #font[12]{pp} 7 TeV (HM)", "LHCb #font[12]{pp} 7 TeV"}, {false, false, false}, 0),
+        // pp : basic mult dependence (manual: mv legend to LHCb label)
+        PlotConfig("radius_mult_pp", ";#font[12]{N_{ch}};#font[12]{R} [fm]", "R : mult (pp)", std::make_pair(0., 100.0), std::make_pair(0.0, 2.5), "radius", {externalPath + "/pp_lhcb_radius_multUnfolded.csv"}, {"#font[12]{pp} 7 TeV"}, {false}, 0),
+        PlotConfig("lambda_mult_pp", ";#font[12]{N_{ch}};#lambda", "#lambda : mult (pp)", std::make_pair(0., 100.0), std::make_pair(0.3, lambdaMax), "lambda", {externalPath + "/pp_lhcb_lambda_multUnfolded.csv"}, {"#font[12]{pp} 7 TeV"}, {false}, 0),
     };
 
     for (const auto &plot : plots)
